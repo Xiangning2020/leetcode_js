2313823dd4e38b976dae59a4128aa563abb3cebd
Xiangning2020 <61442496+Xiangning2020@users.noreply.github.com>
2020-12-16 14:07:55 +0800

    leetcode#70

--- leetcode_70.js
+++ leetcode_70.js
@@ -3,36 +3,37 @@
 // 可以通过数组的方式进行实现。
 // 时间复杂度：O(n), 空间复杂度：O(n)
 
-var climbStairs1 = function (n) {
-  if (n == 1) return 1;
-  if (n == 2) return 2;
+var climbStairs1 = function(n) {
+    if (n == 1) return 1;
+    if (n == 2) return 2;
 
-  let count = [];
-  count[1] = 1;
-  count[2] = 2;
-  for (let i = 3; i <= n; i++) {
-    count[i] = count[i - 1] + count[i - 2];
-  }
-  return count[n];
+    let count = [];
+    count[1] = 1;
+    count[2] = 2;
+    for (let i = 3; i <= n; i++) {
+        count[i] = count[i - 1] + count[i - 2];
+    }
+    return count[n];
 };
 
 console.log(climbStairs1(5));
 
 // 空间复杂度实际上只和两个数字有关，可以降低到常量级O(1)
 
-var climbStairs = function (n) {
-  if (n == 1) return 1;
-  if (n == 2) return 2;
+var climbStairs = function(n) {
+    if (n == 1) return 1;
+    if (n == 2) return 2;
 
-  let a = 1;
-  let b = 2;
-  let c;
-  for (let i = 3; i <= n; i++) {
-    c = a + b;
-    a = b;
-    b = c;
-  }
-  return c;
+    let a = 1;
+    let b = 2;
+    let c;
+    for (let i = 3; i <= n; i++) {
+        c = a + b;
+        [a, b] = [b, c]; // 利用ES6新特性，解构赋值
+        // a = b;
+        // b = c;
+    }
+    return c;
 };
 
 console.log(climbStairs(5));
